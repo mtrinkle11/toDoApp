355532b7023ab727364d7de72eb47cfe307ceab2
Michael Trinkle <mtrinkle11@yahoo.com>
2018-07-08 22:36:14 -0500

    Final for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  todoInput = '';
+  name = 'Mike';
+  todos = [
+    "Take out trash",
+    "Change baby's diaper",
+    "Make dinner",
+    "Clean bathroom",
+    "Do homework",
+    "Read a book"
+  ];
+
+  createTodo() {
+    console.log(this.todoInput);
+    // Pushes text I wrote into the todo array
+    this.todos.push(this.todoInput);
+    // clear out that input
+    this.todoInput = "";
+  }
+
+  completeTodo() {
+
+  }
+  deleteTodo(chore) {
+    console.log('CHORE THAT WAS CLICKED: ' + chore);
+    let index = this.todos.indexOf(chore.todos);
+    console.log(index);
+
+    this.todos[index] = alert(chore + ' Chore deleted!')
+  }
+
+  // deleteTodo(chore:string) {
+  //   console.log('CHORE THAT WAS CLICKED: ' + chore);
+  //   delete[this.todo.indexOf(chore)];
+  //   console.log(chore);
+
+  //   this.todos[chore] = prompt('Chore done!')
+  // }
+
+  editTodo(chore) {
+    console.log('CHORE THAT WAS CLICKED: ' + chore);
+    let index = this.todos.indexOf(chore.todos);
+    console.log(index);
+
+    this.todos[index] = prompt('Please write new todo!')
+  }
+
+
 }
+
